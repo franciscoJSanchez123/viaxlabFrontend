21e3c8f14e20af2a5df44eaa0d2839648e46089c
franciscoJSanchez123 <javisan0105@gmail.com>
2023-03-07 23:34:18 -0430

    deleting code localstorage

--- src/app/services/localStorage-service/local-storage.service.ts
+++ src/app/services/localStorage-service/local-storage.service.ts
@@ -12,18 +12,21 @@ export class LocalStorageService {
 
   constructor() { }
 
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
   async saveActivity(activity:Iactivity){
     this.activity=activity;
-    //this.activities= await this.getActivities()
     this.activities=await this.getActivities()
-    console.log(this.activities,'que es esto')
     this.activities.push(this.activity)
     this.saveActivities(this.activities)
 
   }
-
+//----------------------------------------------------------------------------------------------------------------------
   async updateActivity(activity:Iactivity){
-    console.log('asi llega al local',activity)
+    
     this.activities=await this.getActivities()
     let index = this.activities.findIndex(data => data.activityId === activity.activityId);
     if (index !== -1) {
@@ -37,35 +40,34 @@ export class LocalStorageService {
        
       });
     }
-    console.log('asi queda el arreglo',this.activities)
+    
     this.saveActivities(this.activities)
 
   }
-
+//----------------------------------------------------------------------------------------------------------------------
   saveActivities(activities:Iactivity[]){
 
     localStorage.setItem('activities',JSON.stringify(activities));
     this.activitiesChange.emit()
   }
-
+//----------------------------------------------------------------------------------------------------------------------
 
   async getActivities(){
     this.activitiesAux=  localStorage.getItem('activities');
     this.activities= await JSON.parse(this.activitiesAux);
     return this.activities;
   }
-
+//----------------------------------------------------------------------------------------------------------------------
   deleteAllActivities(){
     localStorage.removeItem("activities");
     this.activitiesChange.emit()
   }
-
+//----------------------------------------------------------------------------------------------------------------------
   async deleteActivity(id:number){
     this.activities=await this.getActivities()
     let index = this.activities.findIndex(activity => activity.activityId === id);
     if (index !== -1)this.activities.splice(index, 1)
     this.saveActivities(this.activities)
-    console.log('se borro ',this.activities)
     
     this.activitiesChange.emit()
    
